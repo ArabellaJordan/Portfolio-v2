a0b0d054f1b1ea8518d8d021f4bec5d49f049626
Arabella Jordan <jordanfaearabella@gmail.com>
2025-07-13 07:18:36 +0800

    landing og

--- Portfolio-v2/src/main.js
+++ Portfolio-v2/src/main.js
@@ -1,4 +1,5 @@
-import "./assets/main.css";
+// import "./assets/main.css";
+import "./assets/scss/style.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
--- Portfolio-v2/src/views/Landing.vue
+++ Portfolio-v2/src/views/Landing.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>Arabella Jordan</div>
+  <div>
+    <div>
+      <div>
+        <h1>Arabella</h1>
+        <h2>Web Developer</h2>
+      </div>
+      <p>lorem ipsum dolor sit amet</p>
+      <nav>
+        <ul>
+          <li>
+            <a>about me</a>
+          </li>
+          <li>
+            <a>projects</a>
+          </li>
+          <li>
+            <a>contact</a>
+          </li>
+          <li>
+            <a>get resume</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div>
+      <img
+        style="width: 300px"
+        src="/public/images/profile.png"
+        alt="profile-image"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
