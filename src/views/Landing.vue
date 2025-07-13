2cd263675986479ec8584081f221e9a9c28f8dd1
Arabella Jordan <jordanfaearabella@gmail.com>
2025-07-14 06:14:26 +0800

    home og

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,38 +1,47 @@
 <template>
   <div>
-    <div>
-      <div>
-        <h1>Arabella</h1>
-        <h2>Web Developer</h2>
+    <section id="home">
+      <div class="home">
+        <div class="home__left">
+          <div class="">
+            <h1 class="text-uppercase">Arabella</h1>
+            <h2>Web Developer</h2>
+          </div>
+          <p class="mt-5 mb-0">lorem ipsum dolor sit amet</p>
+          <div class="d-flex gap-4">
+            <icon :icon="['fab', 'github']" size="2xl" />
+            <icon :icon="['fab', 'linkedin']" size="2xl" />
+            <icon icon="envelope" size="2xl" />
+          </div>
+          <nav class="home__nav">
+            <ul class="home__items">
+              <li class="home__item">
+                <a>about me</a>
+              </li>
+              <li class="home__item">
+                <a>projects</a>
+              </li>
+              <li class="home__item">
+                <a>contact</a>
+              </li>
+              <li class="home__item">
+                <a>get resume</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="home__image">
+          <img :src="profileImage" alt="profile image" />
+        </div>
       </div>
-      <p>lorem ipsum dolor sit amet</p>
-      <nav>
-        <ul>
-          <li>
-            <a>about me</a>
-          </li>
-          <li>
-            <a>projects</a>
-          </li>
-          <li>
-            <a>contact</a>
-          </li>
-          <li>
-            <a>get resume</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    </section>
+
     <div>
-      <img
-        style="width: 300px"
-        src="/public/images/profile.png"
-        alt="profile-image"
-      />
+      <p>scroll down</p>
     </div>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import profileImage from "@/assets/images/profile.png";
 </script>
