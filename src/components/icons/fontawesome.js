d8a433f5d57e4d4b096e8b2e0234485183b043d7
Arabella Jordan <jordanfaearabella@gmail.com>
2025-07-14 06:13:46 +0800

    setup icons

---
+++ src/components/icons/fontawesome.js
@@ -0,0 +1,15 @@
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+/* add icons to the library */
+library.add(faGithub, faEnvelope, faLinkedin);
+
+// Export the component to register globally
+export default FontAwesomeIcon;
--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import "./assets/scss/style.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// import fontawesome icons file
+import FontAwesomeIcon from "./components/icons/fontawesome.js";
+
 // createApp(App).mount("#app");
 
 const app = createApp(App);
+app.component("icon", FontAwesomeIcon);
 app.mount("#app");
