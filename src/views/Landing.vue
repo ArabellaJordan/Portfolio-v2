f6cf438393ba9fe63710bb9d03b30e056e4888c3
Arabella Jordan <jordanfaearabella@gmail.com>
2025-07-22 11:17:36 +0800

    hide scroll up

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -42,7 +42,7 @@
       <div class="arrow"></div>
     </div>
 
-    <div class="scroll__up button__round"><icon icon="arrow-up" /></div>
+    <!-- <div class="scroll__up button__round"><icon icon="arrow-up" /></div> -->
 
     <section id="about">
       <div class="section__container">
